0c15ee19fe2abbfec1ca1e69b91855c3e0a5338c
aligonzalessullon <aligonzalessullon@crackthecode.la>
2025-10-15 21:26:35 -0500

    Primer commit: subir proyecto completo

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/home/Home";
+import Menu from "./pages/menu/Menu";
+import Contact from './pages/contact/Contact.jsx';
+import AboutUs from "./pages/aboutUs/AboutUs.jsx";
+import Books from './pages/books/Books.jsx';
+import Talleres from "./pages/talleres/Talleres.jsx";
+import Header from './components/common/header/Header.jsx';
+import Footer from './components/common/footer/Footer.jsx';
+
+function App() {
+
+  return (
+      <Router>
+        <div className="App">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/aboutUs" element={<AboutUs />} />
+              <Route path='/books' element={<Books />}/>
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/talleres" element={<Talleres />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+
+        
+      </Router>
+  )
+}
+
+export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+
+function Products() {
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("./data/products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const allProducts = [...data.beverages, ...data.desserts];
+        setProducts(allProducts);
+        setFilteredProducts(allProducts); 
+      })
+      .catch((error) => console.log("Error al cargar los productos: ", error));
+  }, []);
+
+ 
+  useEffect(() => {
+    const filtered = products.filter((product) =>
+      product.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  }, [search, products]);
+
+  return (
+    <>
+     
+      <div className="mb-5 flex justify-center">
+        <input
+          type="text"
+          placeholder="Buscar producto..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="bg-background border-2 border-brown-dark outset-border rounded-lg px-3 py-2 w-80"
+        />
+      </div>
+
+      
+      <div className="flex flex-rows flex-wrap justify-center gap-10">
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white border-1 border-brown-dark outset-border w-60 transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:shadow-red rounded-2xl overflow-hidden"
+            >
+              <img src={product.image} alt={product.name} />
+              <div className="flex flex-col p-2">
+                <h3 className="font-bold text-2xl mb-5 text-brown-dark">
+                  {product.name}
+                </h3>
+                <p>{product.description}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-red text-xl mt-5">No se encontraron productos.</p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Products;
